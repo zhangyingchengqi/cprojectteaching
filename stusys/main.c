50e28969c2413052a720854a394ad0128b2fe296
zhangyingchengqi <1069595532@qq.com>
2016-02-29 17:17:32 +0800

    first commit

---
+++ stusys/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	//compile  -> 编译   -> 语法
+	//build  -> 构建
+	// execute -> 运行
+	//printf("hello world!");
+	//定义变量 :   类型 变量名;
+	int choice;
+	do{
+		printf("**********************************************\n");
+		printf("\t湖南皇家工学院学生成绩管理系统\n");
+		printf("\t版本:v1.0\n");
+		printf("\t1.登录\n");
+		printf("\t2.关于我们\n");
+		printf("\t3.关机\n");
+		printf("**********************************************\n");
+		printf("请输入您的选项(1,2,3):\n");
+		scanf("%d",&choice);
+
+		//作业: 
+		//清屏
+		//   判断:   if()   if()...esle if()     switch()
+
+	}while(  1   );
+
+	
+
+}
